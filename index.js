9535f4970615de39200aa9a47312c89545569aef
Kearfy <mt.dev@hotmail.com>
2019-07-26 21:27:52 +0200

    rename

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const fs = require('fmng');
 const updateConfig = () => { fs.write('./config.json', JSON.stringify(config)); }
 const gawk = require('gawk').gawk;
 
-class Jdb {
+class Locdb {
     constructor() {
         this.db = {};
         this.updated = [];
         config.list.forEach((db) => {
-            if (fs.exists(__dirname + '/jdbs/' + db + '.json')) {
-                this.db[db] = gawk(require(__dirname + '/jdbs/' + db + '.json'));
+            if (fs.exists(__dirname + '/files/' + db + '.json')) {
+                this.db[db] = gawk(require(__dirname + '/files/' + db + '.json'));
                 gawk.watch(this.db[db], (newval, oldval) => {
                     this.updated.push(db);
                 });
             } else {
-                console.log('E: jdb.constructor() (initialization) ~> Database "' + db + '" was listed in configuration but its file seems to be missing!');
-                console.log('N: jdb.constructor() (initialization) ~> Automatically creating a new file, but data is gone!');
-                this.log('E: jdb.constructor() (initialization) ~> Database "' + db + '" was listed in configuration but its file seems to be missing!');
-                this.log('N: jdb.constructor() (initialization) ~> Automatically creating a new file, but data is gone!');
-                fs.create(__dirname + '/jdbs/' + db + '.json');
-                fs.write(__dirname + '/jdbs/' + db + '.json', '{}');
+                console.log('E: locdb.constructor() (initialization) ~> Database "' + db + '" was listed in configuration but its file seems to be missing!');
+                console.log('N: locdb.constructor() (initialization) ~> Automatically creating a new file, but data is gone!');
+                this.log('E: locdb.constructor() (initialization) ~> Database "' + db + '" was listed in configuration but its file seems to be missing!');
+                this.log('N: locdb.constructor() (initialization) ~> Automatically creating a new file, but data is gone!');
+                fs.create(__dirname + '/files/' + db + '.json');
+                fs.write(__dirname + '/files/' + db + '.json', '{}');
             }
         });
 
         this.updater = setInterval(() => {
             for (var i = 0; i < this.updated.length; i++) {
                 let db = this.updated[i];
-                fs.write(__dirname + '/jdbs/' + db + '.json', JSON.stringify(this.db[db]));
+                fs.write(__dirname + '/files/' + db + '.json', JSON.stringify(this.db[db]));
             }
             this.updated = [];
         }, config.updateInterval);
@@ -40,7 +40,7 @@ class Jdb {
         this.updater = setInterval(() => {
             for (var i = 0; i < this.updated.length; i++) {
                 let db = this.updated[i];
-                fs.write(__dirname + '/jdbs/' + db + '.json', JSON.stringify(this.db[db]));
+                fs.write(__dirname + '/files/' + db + '.json', JSON.stringify(this.db[db]));
             }
             this.updated = [];
         }, config.updateInterval);
@@ -48,7 +48,7 @@ class Jdb {
 
     exists(db, file = false) {
         if (file) {
-            return fs.exists(__dirname + '/jdbs/' + db + '.json');
+            return fs.exists(__dirname + '/files/' + db + '.json');
         } else {
             return config.list.includes(db);
         }
@@ -56,11 +56,11 @@ class Jdb {
 
     new(db) {
         if (!this.exists(db, true)) {
-            fs.create(__dirname + '/jdbs/' + db + '.json');
+            fs.create(__dirname + '/files/' + db + '.json');
         }
 
-        if (fs.read(__dirname + '/jdbs/' + db + '.json') === '') {
-            fs.write(__dirname + '/jdbs/' + db + '.json', '{}');
+        if (fs.read(__dirname + '/files/' + db + '.json') === '') {
+            fs.write(__dirname + '/files/' + db + '.json', '{}');
         }
 
         if (!this.exists(db)) {
@@ -68,21 +68,17 @@ class Jdb {
             updateConfig();
         }
 
-        this.db[db] = gawk(require(__dirname + '/jdbs/' + db + '.json'));
+        this.db[db] = gawk(require(__dirname + '/files/' + db + '.json'));
         gawk.watch(this.db[db], (newval, oldval) => {
             this.updated.push(db);
         });
     }
 
-    purge(path, confirm = false) {
+    purge(path) {
         if (this.exists(path)) {
-            if (confirm) {
-                fs.remove(__dirname + '/jdbs/' + path + '.json');
-            } else {
-                console.log('N: jdb.purge() ~> Did not delete database, no confirmation given in. Use jdb.purge(path, true) instead.');
-            }
+            fs.remove(__dirname + '/files/' + path + '.json');
         } else {
-            console.log('E: jdb.purge() ~> DB "' + path + '" does not exist, aborted deletion!');
+            console.log('E: locdb.purge() ~> DB "' + path + '" does not exist, aborted deletion!');
         }
     }
 
@@ -93,14 +89,14 @@ class Jdb {
             var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 
             try {
-                if (!fs.exists(__dirname + '/logs/' + today + '.jdb.log')) { fs.mk(__dirname + '/logs/' + today + '.jdb.log'); }
-                fs.append(__dirname + '/logs/' + today + '.jdb.log', '[' + time + '] ~ ' + msg + "\n");
+                if (!fs.exists(__dirname + '/logs/' + today + '.locdb.log')) { fs.mk(__dirname + '/logs/' + today + '.locdb.log'); }
+                fs.append(__dirname + '/logs/' + today + '.locdb.log', '[' + time + '] ~ ' + msg + "\n");
             } catch(e) {
-                console.log('JDB ERROR: COULD NOT WRITE TO LOG! LOGGING ERROR(S).', e);
+                console.log('LOCDB ERROR: COULD NOT WRITE TO LOG! LOGGING ERROR(S).', e);
                 return false;
             }
         } else {
-            console.log('JDB ERROR: LOG MESSAGE MUST BE GIVEN IN!');
+            console.log('LOCDB ERROR: LOG MESSAGE MUST BE GIVEN IN!');
             return false;
         }
 
@@ -108,4 +104,4 @@ class Jdb {
     }
 }
 
-module.exports = new Jdb();
+module.exports = new Locdb();
